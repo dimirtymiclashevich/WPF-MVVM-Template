4252ae45d55b8e61fc36fc1bc27185efa7bf766d
dimirty miclashevich <miclashevich@gmail.com>
2018-05-21 19:55:36 +0300

    DemoViewModel added

--- WPF.MVVM.Template/MainWindow.xaml.cs
+++ WPF.MVVM.Template/MainWindow.xaml.cs
@@ -1,25 +1,25 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using AppName.Common;
 
 namespace AppName
 {
     public partial class MainWindow : Window
     {
+        private readonly BouncingSuppressor _bouncingSuppressor = new BouncingSuppressor();
+
         public MainWindow()
         {
             InitializeComponent();
+            SizeChanged += OnSizeChanged;
+        }
+
+        private async void OnSizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            if (await _bouncingSuppressor.Check())
+            {
+                // some actions
+            }
         }
     }
 }
---
+++ WPF.MVVM.Template/ViewModels/DemoViewModel.cs
@@ -0,0 +1,29 @@
+﻿using AppName.Models;
+using System;
+using System.Collections.ObjectModel;
+
+
+namespace AppName.ViewModels
+{
+    public class DemoViewModel : ViewModelBase
+    {
+        private string _header;
+
+        public DemoViewModel()
+        {
+            Header = "Header";
+            for (int i = 0; i < 20; i++)
+            {
+                UserList.Add(new User() { FirstName = i.ToString(), SecondName = (i * 10).ToString() });
+            }
+        }
+
+        public ObservableCollection<User> UserList { get; private set; } = new ObservableCollection<User>();
+
+        public string Header
+        {
+            get { return _header; }
+            set { SetProperty(ref _header, value); }
+        }
+    }
+}
